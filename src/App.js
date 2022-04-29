c5f8a754f25a052389733a76ad520150360b0838
nate <nathan.kwok@jimmybrings.com.au>
2022-04-29 15:32:47 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>todoList</h1>
+      <li>fix conflicts with JBO-137 PR</li>
+      <li>Show list to KK</li>
+      <button>Add stuff to do to list</button>
+    </>
   );
 }
 
